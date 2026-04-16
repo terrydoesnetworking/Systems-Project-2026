3341f3c70bc5c1d72b56209e9f506a7ddb31a887
Terry Perkins <terrydoesnetworking@gmail.com>
2026-04-15 22:38:26 -0500

    Got the picture to work finally

--- system-project-2026/src/lib/Header.svelte
+++ system-project-2026/src/lib/Header.svelte
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary sticky-top">
     <div class="container-fluid">
-      <img src="images/Logo.svg" alt="Logo" style="max-width:50px;width:100%"
+      <img src="/Logo.svg" alt="Logo" style="max-width:50px;width:100%"
         class="d-inline-block align-text-top navbar-brand">
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
         aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -17,6 +17,9 @@
           <li class="nav-item">
             <a class="nav-link" href="/slides">Slides</a>
           </li>
+          <!-- <li class="nav-item">
+            <a class="nav-link" href="/demo">Demo</a>
+          </li> -->
         </ul>
         <ul class="nav navbar-nav navbar-right">
           <li><a href="/login" class="nav-link">Login</a></li>
