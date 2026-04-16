0edfd79ad0dbbe70a59e209b429ef343db3b2ab5
Terry Perkins <terrydoesnetworking@gmail.com>
2026-04-16 15:22:08 -0500

    tried working on setting up the popup

--- system-project-2026/src/routes/pwned/+page.svelte
+++ system-project-2026/src/routes/pwned/+page.svelte
@@ -1,14 +1,101 @@
-<div> 
-    <h1 class="text-3xl font-bold">
-        YOU JUST GOT PWNED!!!!
-    </h1>
 
-    <p> Visit 
-        <a href="https://haveibeenpwned.com/" class="text-blue-400" target="_blank">haveibeenpwned.com</a>
-        and check to see if your email has been involved in any data breaches.
-    </p>
-    <p> We also would recommend checking out our training services by clicking
-        <a href="/" class="text-blue-400">here</a>
-    </p>
-        <p class="font-bold"> !you will be redirected and cannot return!</p>
-</div>
+
+
+<!-- <script>
+  // svelte-ignore non_reactive_update
+    let showPopup = true;
+
+  function accept() {
+    showPopup = false;
+  }
+</script>
+
+{#if showPopup} -->
+  <!-- OVERLAY -->
+  <!-- <div
+    id="overlay"
+    style="
+      position: fixed;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background: rgba(0, 0, 0, 0.5);
+      z-index: 1000;
+    "
+  ></div> -->
+
+  <!-- POPUP -->
+  <!-- <div
+    id="popup"
+    style="
+      position: fixed;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      background: orange;
+      padding: 20px;
+      border-radius: 10px;
+      z-index: 1001;
+      text-align: center;
+    "
+  > -->
+    <!-- BIG TEXT -->
+    <!-- <h1 style="color: red; margin-bottom: 10px;">You Got Pwned</h1> -->
+
+    <!-- SMALL TEXT -->
+    <!-- <p style="font-size: 14px;">
+      This site will show and explain how to prevent this from happening.
+    </p> -->
+
+    
+    <!-- <button on:click={accept}>Continue</button>
+  </div>
+{/if} -->
+
+
+
+
+
+<!-- OVERLAY -->
+<!-- <div
+  id="overlay"
+  class="fixed top-0 left-0 hidden h-full w-full bg-black/50"
+></div> -->
+
+<!-- POPUP -->
+<!-- <div
+  id="popup"
+  class="fixed top-1/2 left-1/2 hidden -translate-x-1/2 -translate-y-1/2 rounded-2xl bg-orange-500 p-5"
+> -->
+  <!-- BIG TEXT -->
+  <!-- <h1 class="mb-2.5 text-2xl font-bold text-red-600">You Got Pwned</h1> -->
+
+  <!-- SMALL TEXT -->
+  <!-- <p class="mb-4 text-sm text-black">
+    This site will show and explain how to prevent this from happening.
+  </p>
+
+  <button  
+    class="rounded bg-white px-4 py-2 text-sm font-semibold text-black hover:bg-gray-200"
+  >
+    Continue
+  </button>
+</div> -->
+
+<!-- <script>
+  function accept() {
+    // @ts-ignore
+    document.getElementById("popup").classList.add("hidden");
+    // @ts-ignore
+    document.getElementById("overlay").classList.add("hidden");
+  }
+
+  //  THIS MAKES IT AUTO POP UP
+  window.onload = function () {
+    // @ts-ignore
+    document.getElementById("popup").classList.remove("hidden");
+    // @ts-ignore
+    document.getElementById("overlay").classList.remove("hidden");
+  };
+</script> -->
